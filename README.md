1b9e73938c6ae1ad753889ff834b14174a15fb38
Eric Fried <efried@redhat.com>
2021-05-14 12:46:40 -0500

    Bootstrap the project
    Initial commit adding and/or moving over from
    [splunk-forwarder-operator](https://github.com/openshift/splunk-forwarder-operator/tree/c28e0fb044330d413e18cd7f0a43ab1673d2aa18):
    - Dockerfiles and container entrypoints
    - Version and hash files
    - Prow root image config
    - app-sre pipeline script
    - Vulnerability check script
    - Makefile content
    - Documentation
    
    OSD-7269

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # splunk-forwarder-images
-Build and push splunk universal forwarder images
+Build and push container images for splunk universal forwarder.
+
+## Local Build/Test
+The following `make` variables affect building/pushing:
+- `IMAGE_REGISTRY` (default `quay.io`)
+- `IMAGE_REPOSITORY` (default `app-sre`)
+- `FORWARDER_NAME` (default `splunk-forwarder`)
+- `HEAVYFORWARDER_NAME` (default `splunk-heavyforwarder`)
+
+Images will be tagged and pushed as `${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}/${[HEAVY]FORWARDER_NAME}:${VERSION}-${HASH}`, where `${VERSION}` and `${HASH}` are gleaned from [.splunk-version](.splunk-version) and [.splunk-version-hash](.splunk-version-hash), respectively.
+Therefore, for local building and testing, you should create personal image repositories and point to them by overriding at least `IMAGE_REPOSITORY`.
+
+Build images using `make build-forwarder` and `make build-heavyforwarder`.
+
+Push images using `make push-forwarder` and `push-heavyforwarder`.
+
+Run vulnerability checks using `make vuln-check`.
+
+## Versioning and Tagging
+This repository builds container images around the splunk universal forwarder at the version and hash specified in the [.splunk-version](.splunk-version) and [.splunk-version-hash](.splunk-version-hash) files, respectively.
+To build around a new version, simply commit a PR updating those files.
+
+## CICD
+After a PR merges, an integration job is run by app-sre triggering build/push of both images to `quay.io/app-sre/splunk-(heavy)forwarder`.
+Images are only built and pushed if they do not already exist in the repository at the current version.
+This is to avoid the "disappearing digest" problem, facilitating by-digest pulls of these images.
+
+To test the app-sre pipeline:
+- Create personal repositories and override variables as described [above](#local-buildtest).
+- Obtain credentials from your personal repository and set the `REGISTRY_USER` and `REGISTRY_TOKEN` variables.
+- Run `make build-push`.
---
+++ hack/app-sre-build-push.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+set -e
+
+usage() {
+    cat <<EOF
+Usage: $0 IMAGE_URI DOCKERFILE_PATH
+
+If IMAGE_URI doesn't already exist in the remote repository, build it
+via the Dockerfile at DOCKERFILE_PATH, and tag & push it to IMAGE_URI.
+
+Specify IMAGE_URI by tag, e.g.:
+    quay.io/app-sre/splunk-forwarder:8.0.5-a1a6394cc5ae
+EOF
+    exit -1
+}
+
+## image_exits_in_repo IMAGE_URI
+#
+# Checks whether IMAGE_URI -- e.g. quay.io/app-sre/osd-metrics-exporter:abcd123
+# -- exists in the remote repository.
+# If so, returns success.
+# If the image does not exist, but the query was otherwise successful, returns
+# failure.
+# If the query fails for any reason, prints an error and *exits* nonzero.
+image_exists_in_repo() {
+    local image_uri=$1
+    local output
+    local rc
+
+    local skopeo_stderr=$(mktemp)
+
+    output=$(skopeo inspect docker://${image_uri} 2>$skopeo_stderr)
+    rc=$?
+    # So we can delete the temp file right away...
+    stderr=$(cat $skopeo_stderr)
+    rm -f $skopeo_stderr
+    if [[ $rc -eq 0 ]]; then
+        # The image exists. Sanity check the output.
+        local digest=$(echo $output | jq -r .Digest)
+        if [[ -z "$digest" ]]; then
+            echo "Unexpected error: skopeo inspect succeeded, but output contained no .Digest"
+            echo "Here's the output:"
+            echo "$output"
+            echo "...and stderr:"
+            echo "$stderr"
+            exit 1
+        fi
+        echo "Image ${image_uri} exists with digest $digest."
+        return 0
+    elif [[ "$stderr" == *"manifest unknown"* ]]; then
+        # We were able to talk to the repository, but the tag doesn't exist.
+        # This is the normal "green field" case.
+        echo "Image ${image_uri} does not exist in the repository."
+        return 1
+    elif [[ "$stderr" == *"was deleted or has expired"* ]]; then
+        # This should be rare, but accounts for cases where we had to
+        # manually delete an image.
+        echo "Image ${image_uri} was deleted from the repository."
+        echo "Proceeding as if it never existed."
+        return 1
+    else
+        # Any other error. For example:
+        #   - "unauthorized: access to the requested resource is not
+        #     authorized". This happens not just on auth errors, but if we
+        #     reference a repository that doesn't exist.
+        #   - "no such host".
+        #   - Network or other infrastructure failures.
+        # In all these cases, we want to bail, because we don't know whether
+        # the image exists (and we'd likely fail to push it anyway).
+        echo "Error querying the repository for ${image_uri}:"
+        echo "stdout: $output"
+        echo "stderr: $stderr"
+        exit 1
+    fi
+}
+
+[[ $# -eq 2 ]] || usage
+
+image_uri=$1
+dockerfile_path=$2
+
+if image_exists_in_repo "${image_uri}"; then
+    echo "Skipping build/push for ${image_uri}"
+else
+    # build and push the image
+    make IMAGE_URI="${image_uri}" DOCKERFILE_PATH="${dockerfile_path}" docker-build-push-one
+fi
